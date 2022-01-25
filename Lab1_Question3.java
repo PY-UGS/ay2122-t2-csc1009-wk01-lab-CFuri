361bf24b1123419935615576d2ac696fcbbca35c
CFuri <92706052+CFuri@users.noreply.github.com>
2022-01-25 23:41:56 +0800

    Add files via upload

---
+++ Lab1_Question3.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Lab1_Question3 {
+	public static void main(String arg[])   
+    {   
+        for(int x = 102; x > 66; x = x-1)
+        {
+        	if(x % 2 == 1)
+        	{
+        		System.out.print("value of x: "+x);
+        	System.out.print("\n");
+        	}
+        }
+    }
+}
